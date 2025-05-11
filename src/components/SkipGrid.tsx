4563e216edd0b0fc250eab6b58bd4c807eb08e1c
SandroSimic <sandrosimic8@gmail.com>
2025-05-11 13:45:49 +0200

    first commit

--- src/components/SkipGrid.tsx
+++ src/components/SkipGrid.tsx
@@ -24,7 +24,7 @@ function SkipGrid({
           setLoading(false);
         }, 1000);
       } catch (err) {
-        console.error("Failed to fetch skips:", err);
+        console.error("Failed to fetch skips", err);
         setLoading(false);
       }
     };
